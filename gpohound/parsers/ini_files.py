a1bc8211a91048e94224de138c0a07d9beea6131
dadevel <dadevel@disroot.org>
2025-04-25 09:10:50 +0200

    include file path in ini parsing errors

--- gpohound/parsers/ini_files.py
+++ gpohound/parsers/ini_files.py
@@ -22,14 +22,17 @@ class INIParser:
         """
         filename = os.path.basename(file_path).lower()
 
-        if filename == "gpt.ini":
-            return self._parse_gpt(file_path)
-        elif filename == "scripts.ini":
-            return self._parse_scripts(file_path)
-        elif filename == "psscripts.ini":
-            return self._parse_psscripts(file_path)
-        else:
-            return None
+        try:
+            if filename == "gpt.ini":
+                return self._parse_gpt(file_path)
+            elif filename == "scripts.ini":
+                return self._parse_scripts(file_path)
+            elif filename == "psscripts.ini":
+                return self._parse_psscripts(file_path)
+            else:
+                return None
+        except Exception as e:
+            raise ValueError(f'parser error in {file_path}: {e}') from e
 
     def _parse_gpt(self, file_path):
         """Parses GPT.ini based on the YAML configuration."""
